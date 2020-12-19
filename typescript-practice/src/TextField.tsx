85db6807d7c3af398946ef29343bed1cf3f36424
Luis PM <lpm@pop-os.localdomain>
2020-12-19 13:17:03 -0500

    handle change with typescritp and interfaces

--- typescript-practice/src/TextField.tsx
+++ typescript-practice/src/TextField.tsx
@@ -12,21 +12,23 @@ interface Props {
   interger?: number;
   fn?: (bob: string) => string;
   person: Person;
+  handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 interface TextNode {
   text: string;
 }
 
-export const TextField: React.FC<Props> = ({ text }) => {
+export const TextField: React.FC<Props> = ({ text, handleChange }) => {
   const [count, setcount] = useState<TextNode>({ text: "hello" });
 
   const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div>
+    <div ref={divRef}>
       <h1>{text}</h1>
-      <input ref={inputRef} />
+      <input ref={inputRef} onChange={handleChange} />
     </div>
   );
 };
